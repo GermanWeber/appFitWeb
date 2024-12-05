36f819c3f58a8ca45427c141b7753d0e7b19e869
german <ge.weber@duocuc.cl>
2024-12-05 19:26:59 -0300

    agente agregado

--- appFit/views.py
+++ appFit/views.py
@@ -5,3 +5,20 @@ from django.shortcuts import render
 
 def pagPrincipal(request):
     return render(request, "appFit/index.html")
+
+
+def inicio(request):
+    user_agent = request.META["HTTP_USER_AGENT"]
+    is_mobile = any(
+        device in user_agent.lower()
+        for device in [
+            "iphone",
+            "android",
+            "ipad",
+            "ipod",
+            "blackberry",
+            "iemobile",
+            "opera mini",
+        ]
+    )
+    return render(request, "index.html", {"is_mobile": is_mobile})
