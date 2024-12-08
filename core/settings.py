b2e658c8923429f8f15fb3e15fa3a12aa12cc57b
german <ge.weber@duocuc.cl>
2024-12-08 12:14:07 -0300

    subir a render

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure--+q=r-7&foazdxhmd8g%+i9iki^b-l7$6g302**n(8pn7em)lr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["appFitWeb.onrender.com"]
 
 
 # Application definition
@@ -124,3 +124,6 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Asegúrate de definir esta ruta
